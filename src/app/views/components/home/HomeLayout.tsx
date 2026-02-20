a9e8e3daba43c90572c010fbbe07f4bd6f0e7754
OnurArk <onurark11@gmail.com>
2026-02-20 23:55:04 +0300

    refactor: simplify HomeLayout by removing unnecessary max-width class

--- src/app/views/components/home/HomeLayout.tsx
+++ src/app/views/components/home/HomeLayout.tsx
@@ -3,7 +3,7 @@ import { HeroSection, GradientExamples, GettingStarted, FeatureCards } from ".";
 export default function HomeLayout() {
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 center">
-      <div className="max-w-4xl mx-auto p-8">
+      <div className="p-8">
         <HeroSection />
         <GradientExamples />
         <GettingStarted />
