18acad2a660b3548b89cfac1c536fee00af19035
OnurArk <onurark11@gmail.com>
2026-02-20 23:47:10 +0300

    feat: introduce AuthPage and AuthLayout components, and update HomePage layout import

--- src/app/views/components/home/index.ts
+++ src/app/views/components/home/index.ts
@@ -2,4 +2,4 @@ export { default as HeroSection } from "./HeroSection";
 export { default as GradientExamples } from "./GradientExamples";
 export { default as GettingStarted } from "./GettingStarted";
 export { default as FeatureCards } from "./FeatureCards";
-export { default as PageLayout } from "./PageLayout";
+export { default as PageLayout } from "./HomeLayout";
